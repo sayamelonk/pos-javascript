2e8d6b5e0afe9831d61051441a39d2b45b179f37
ikylonk <dayatketangga95@gmail.com>
2025-04-12 10:33:56 +0800

    first commit

--- backend-pos/index.js
+++ backend-pos/index.js
@@ -1,8 +1,23 @@
 // import express
 const express = require("express");
 
+// import cors
+const cors = require("cors");
+
+// import bodyParser
+const bodyParser = require("body-parser");
+
 // init app
 const app = express();
+
+// use cors
+app.use(cors());
+
+// use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // define port
 const port = 3000;
@@ -15,4 +30,4 @@ app.get("/", (req, res) => {
 // start server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-})
+});
