7225ee38ac16f79edf8c281d88fd73bc5e7641e2
ikylonk <dayatketangga95@gmail.com>
2025-04-16 14:41:36 +0800

    Setup Prisma ORM (Database)

--- backend-pos/prisma/migrations/20250412084428_init/migration.sql
+++ backend-pos/prisma/migrations/20250412084428_init/migration.sql
@@ -17,7 +17,7 @@ CREATE TABLE `categories` (
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updated_ar` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
