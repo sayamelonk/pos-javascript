b83301cc54a477c036a6c4a8bd7d0fde014dc022
ikylonk <dayatketangga95@gmail.com>
2025-05-08 14:12:07 +0800

    refactor utils

--- backend-pos/utils/moneyFormat.js
+++ backend-pos/utils/moneyFormat.js
@@ -2,6 +2,8 @@ const moneyFormat = (number) => {
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   }).format(number);
 };
 
